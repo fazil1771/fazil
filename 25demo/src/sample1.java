012144f4944140271ea21bb92d9fd10248f32d9e
LENOVO <LENOVO@fazil>
2022-01-29 14:55:16 +0530

    aaaaaa

---
+++ 25demo/src/sample1.java
@@ -0,0 +1,19 @@
+
+
+public class sample1
+{
+	public static void main(String[] args){
+		 int num1=100;
+		 int num2=25;
+		System.out.println("the result for the following are ");
+	      int mul = num1*num2;
+	    System.out.println("multiplication of this two number is...:"+ mul);
+	int div = num1/num2;
+	     System.out.println("the divison of this two useless integers is..:"+ div);
+	    // operators how to use them
+	    //mod operator
+	     int mod =num1%num2;
+	    		 System.out.println("the remainder is going to be...:"+ mod);
+	     
+	}
+ }
